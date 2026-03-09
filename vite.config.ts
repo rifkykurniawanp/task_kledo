ef7b3109444eeb3592563e65c095e0d832bc1f3b
Rifky Kurniawan Putra <krifky14@gmail.com>
2026-03-10 00:26:21 +0700

    fix: comment out base URL in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/task_kledo/",
+  // base: "/task_kledo/",
   plugins: [
     tailwindcss(),
     react({
